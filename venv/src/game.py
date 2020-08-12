4ec188c0b9371676411c84b4cad447e138f98aa5
AnabelSalomone <anabel.salomone@gmail.com>
2020-08-12 22:07:22 +0200

    get name

--- venv/src/game.py
+++ venv/src/game.py
@@ -1,5 +1,27 @@
-player_name = input("What's your name?")
-player_attack = 10
-player_heal = 16
-health = 100
+player = {
+    "name": "Player 1",
+    "attack": 10,
+    "heal": 16,
+    "health": 100
+}
+
+monster = {
+    "name": "Max",
+    "attack": 12,
+    "health": 100
+}
+
+#Get User's name
+player["name"] = input("What's your name: ")
+
+while len(player["name"]) < 1:
+    player["name"] = input("Please write a name: ")
+
+
+#Action selection
+print("Select an action")
+print("1 - Attack")
+print("2 - Heal")
+
+player_choice = input("What do you choose?")
 
